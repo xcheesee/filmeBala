d55a99a1042b737895e7fa48a09499e7a946ba1b
Joao Lucas Silva <joaolucas@prefeitura.sp.gov.br>
2022-12-22 11:44:47 -0300

    criado layout de pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,43 @@
 import { type NextPage } from "next";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { TypeOf } from "zod";
 
 const Home: NextPage = () => {
+
     return(
-        <div className="min-w-screen relative transition duration-1000 -translate-x-full overflow-hidden">
-            <Image src='/wp1945898.jpg' fill alt="" />
-            <Image src='/wp1945898.jpg' fill alt="" />
-            <Image src='/wp1945898.jpg' fill alt="" />
-        </div>
+        <>
+            <div className="w-full h-screen relative overflow-hidden">
+                <div className="absolute z-10 h-full w-full bg-[rgba(0,0,0,0.85)] flex flex-col justify-center items-center gap-4">
+                    <p className="text-7xl tracking-widest font-light  py-8">SO FILME BALA</p>
+                    <p className="text-3xl">De Mafioso, De Guerra, De Luta, De Tiro, etc. so os bom mlk.</p>
+                    <p className="text-3xl">Vem de arrasta pra cima que eh sucesso, tudo gratis.</p>
+                </div>
+                <div className={`min-h-full flex relative  infiniteScroll`}>
+                    <div className="relative min-w-full">
+                        <Image src='/wp1945898.jpg' fill alt=""/>
+                    </div>
+                    <div className="relative min-w-full">
+                        <Image src='/wp1945898.jpg' fill alt=""/>
+                    </div>
+                    <div className="relative min-w-full">
+                        <Image src='/wp1945898.jpg' fill alt=""/>
+                    </div>
+                </div>
+            </div>
+            <div className="py-8 lg:px-4">
+                <p>Populares</p>
+                <div className="w-full h-[300px] bg-red-200"></div>
+            </div>
+            <div className="py-8 lg:px-4">
+                <p>Tiro</p>
+                <div className="w-full h-[300px] bg-red-200"></div>
+            </div>
+            <div className="py-8 lg:px-4">
+                <p>Guerra</p>
+                <div className="w-full h-[300px] bg-red-200"></div>
+            </div>
+        </>
     )
 }
 
