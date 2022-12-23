d38ba36be929009e6076b639671d4d7a05a14802
Joao Lucas Silva <joaolucas@prefeitura.sp.gov.br>
2022-12-23 13:55:22 -0300

    alterado estilo de mCard

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,21 @@ import Image from "next/image"
 
 const Footer: React.FC = () => {
     return (
-        <div className="flex justify-center p-4 gap-2">
-            <p className="flex gap-1">Made by<a href="https://github.com/xcheesee" className="flex gap-1">
-                Lucas Silva <Image src="/github-mark-white.png" alt="" width={24} height={24}/>
-            </a></p>
+        <div className="max-w-[100vw]">
+            <div className="flex justify-center  p-4 gap-2">
+                <div className="flex gap-1">Made by<a href="https://github.com/xcheesee" className="flex gap-1">
+                    <p>Lucas Silva</p> <Image src="/github-mark-white.png" alt="" width={24} height={24}/>
+                </a>
+            
+                </div>
+            </div>
+            <div className="flex justify-center  p-4 gap-2">
+                <div className="flex gap-1">Powered by<a href="https://www.themoviedb.org/" className="flex gap-1">
+                    <Image src="/tmdb.svg" alt="" width={124} height={24}/>
+                </a>
+            
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -9,7 +9,9 @@ const MovieCard: React.FC<McardProps> =  ({ name, playtime, description, ratings
             <div className="mt-auto relative w-full h-full translate-y-[80%] hover:translate-y-[0%] transition bg-neutral-800 relative rounded duration-1000 mCard grid grid-cols-3 grid-rows-[min-content_min-content_1fr_min-content]">
                 <p className="col-span-3 text-xl">{name}</p>
                 <p className="row-start-2 text-neutral-500">{playtime}</p>
-                <p className="row-start-3 self-center col-span-3 text-sm overflow-auto">{description}</p>
+                <div className='row-start-3 col-span-3 overflow-hidden overflow-y-auto my-4'>
+                    <p className="h-full text-sm align-middle">{description}</p>
+                </div>
                 <p className="row-start-4">{ratings} <FontAwesomeIcon icon={faStar} /></p>
                 <p className="row-start-5 col-span-3">Botao Ver / Botao Assistir Depois</p>
 
