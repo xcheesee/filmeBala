3cb878cb181c7cc850d1b5932d0fc8a0b5619cf9
dorival <liscaDoido@gmail.com>
2023-02-20 23:10:19 -0300

    cors?

--- src/pages/api/movies/populares.ts
+++ src/pages/api/movies/populares.ts
@@ -2,12 +2,11 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import { env } from "../../../env/server.mjs";
 
 const getPopular = async (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
     const { genre } = req.query
-
-    const data = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${env.TMDB_API_KEY}&language=en-US&with_genres=${genre}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
-    // await new Promise((res,rej) => {
-    //     setTimeout(() => res('pog'), 3000)
-    // })
+    const data = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${env.TMDB_API_KEY}&language=en-US&with_genres=${genre}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`, {
+        mode: "cors"
+    })
     const json = await data.json()
     res.send(json)
 }
