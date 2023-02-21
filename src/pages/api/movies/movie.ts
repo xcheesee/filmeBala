74fd7f09c1ac8785c450680d815615c78d55bbca
dorival <liscaDoido@gmail.com>
2023-02-20 23:00:19 -0300

    cors vercel

--- src/pages/api/movies/movie.ts
+++ src/pages/api/movies/movie.ts
@@ -2,7 +2,6 @@ import { type NextApiResponse, type NextApiRequest } from "next";
 import { env } from "../../../env/server.mjs";
 
 const movie = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
     const { id } = req.query
     const data = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${env.TMDB_API_KEY}&language=en-US`)
     const json = await data.json()
