623c654269fb488702bb84d6fefdc7d6c84b79c6
dorival <liscaDoido@gmail.com>
2023-03-18 11:41:40 -0300

    melhorado ux em profile mobile

--- src/components/sidebarElement.tsx
+++ src/components/sidebarElement.tsx
@@ -8,9 +8,9 @@ const SidebarElement: React.FC<{
 }> = ({children, tabValue, setTab, setDrawer}) => {
     return(
         <div className="mr-8">
-            <div className="border-red-500 -right-10 relative rounded-r sidebar" style={{background: GRADIENT_COLOR_TOP_BOTTOM}}>
+            <div className={`border-red-500 md:-right-10 relative rounded-r sidebar max-md:text-center flex flex-col max-md:justify-between`} style={{background: GRADIENT_COLOR_TOP_BOTTOM}}>
                     <div 
-                        className="bg-neutral-900 relative right-2 py-2 cursor-pointer"
+                        className="bg-neutral-900 relative md:right-2 py-2 cursor-pointer"
                         onClick={() => {
                             setTab(tabValue)
                             setDrawer(false)
